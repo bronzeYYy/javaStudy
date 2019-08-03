0af023c84580a4c4c0852e7f55e3ea9d12f05645
1769178812@qq.com <1769178812@qq.com>
2019-08-03 19:55:59 +0800

    更新了ThreadStateDemo注释和对应的pdf说明

--- src/thread/ThreadStateDemo.java
+++ src/thread/ThreadStateDemo.java
@@ -7,14 +7,19 @@ public class ThreadStateDemo implements Runnable{
     public void run() {
         synchronized (o) {
             try {
+                // 线程0到达此处，线程1等待线程0释放锁，线程2状态为BLOCKED
                 Thread.sleep(300L); // 语句1
-                o.wait();
+                // 线程0到达此处，状态为WAITING并释放锁，线程1获取到锁执行语句1
+                o.wait(100);
+                // 线程1还未到达wait()时，主线程唤醒线程0，线程0继续执行
                 Thread.sleep(100L); // 语句3
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         }
     }
+
+
 
     public static void main(String[] args) {
         Thread thread = new Thread(new ThreadStateDemo(), "线程0");
@@ -36,10 +41,21 @@ public class ThreadStateDemo implements Runnable{
         }
         System.out.println(thread.getName() + thread.getState()); // 拿到锁的线程进入语句3，另一个线程则BLOCKED
         try {
-            thread.join(); // 等待执行完毕
+            thread.join(); // 等待执行完毕，主线程状态为TIMED_WAITING
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+
+        /*synchronized(thread) {
+            while (thread.isAlive()) {
+                // 不停的进行判断，防止被假唤醒
+                try {
+                    thread.wait(0);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }*/
         System.out.println(thread.getName() + thread.getState()); // TERMINATED
 
     }
