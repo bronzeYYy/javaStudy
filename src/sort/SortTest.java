6ed7ff3a25758186108e82de8f07d7d0ea9ec8bd
1769178812@qq.com <1769178812@qq.com>
2019-08-13 22:00:36 +0800

    堆排序

--- src/sort/SortTest.java
+++ src/sort/SortTest.java
@@ -1,16 +1,28 @@
 package sort;
 
 import design.factory.SimpleSortFactory;
+import design.factory.factory.HeadSortFactory;
+import design.factory.factory.SortFactory;
 import design.template.SortTestTemplate;
+
+import java.util.Comparator;
 
 public class SortTest {
     public static void main(String[] args) {
-        SortTestTemplate sortTestTemplate = new SortTestTemplate() {
+        /*SortTestTemplate sortTestTemplate = new SortTestTemplate() {
             @Override
             protected Sort getSort() {
                 return SimpleSortFactory.getSort("qSort");
             }
         };
-        sortTestTemplate.test(200);
+        sortTestTemplate.test(200);*/
+        SortFactory sortFactory = new HeadSortFactory();
+        SortTestTemplate sortTestTemplate = new SortTestTemplate() {
+            @Override
+            protected Sort getSort() {
+                return sortFactory.create();
+            }
+        };
+        sortTestTemplate.test(100);
     }
 }
